afb311badd9c9ae3a4b2eb503a31687a1e80319f
siddhant-pandya <siddhant0804@gmail.com>
2020-12-06 23:43:20 +0530

    added views

--- ocr/ocrapp/views.py
+++ ocr/ocrapp/views.py
@@ -3,8 +3,138 @@ from django.http import HttpResponse
 import csv
 import cv2
 import pytesseract
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+import numpy as np
+
+def pre_processing(image):
+    """
+    This function take one argument as
+    input. this function will convert
+    input image to binary image
+    :param image: image
+    :return: thresholded image
+    """
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # converting it to binary image
+    threshold_img = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    # saving image to view threshold image
+    cv2.imwrite('thresholded.png', threshold_img)
+
+    #cv2.imshow('threshold image', threshold_img)
+    # Maintain output window until
+    # user presses a key
+    cv2.waitKey(0)
+    # Destroying present windows on screen
+    #cv2.destroyAllWindows()
+
+    return threshold_img
+
+
+def parse_text(threshold_img):
+    """
+    This function take one argument as
+    input. this function will feed input
+    image to tesseract to predict text.
+    :param threshold_img: image
+    return: meta-data dictionary
+    """
+    # configuring parameters for tesseract
+    tesseract_config = r'--oem 3 --psm 6'
+    # now feeding image to tesseract
+    details = pytesseract.image_to_data(threshold_img, output_type=pytesseract.Output.DICT,
+                                        config=tesseract_config, lang='eng')
+    return details
+
+
+def draw_boxes(image, details, threshold_point):
+    """
+    This function takes three argument as
+    input. it draw boxes on text area detected
+    by Tesseract. it also writes resulted image to
+    your local disk so that you can view it.
+    :param image: image
+    :param details: dictionary
+    :param threshold_point: integer
+    :return: None
+    """
+    total_boxes = len(details['text'])
+    for sequence_number in range(total_boxes):
+        if int(details['conf'][sequence_number]) > threshold_point:
+            (x, y, w, h) = (details['left'][sequence_number], details['top'][sequence_number],
+                            details['width'][sequence_number], details['height'][sequence_number])
+            image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # saving image to local
+    
+    #cv2.imwrite('captured_text_area.png', image)
+    
+    # display image
+    #cv2.imshow('captured text', image)
+    # Maintain output window until user presses a key
+    #cv2.waitKey(0)
+    # Destroying present windows on screen
+    #cv2.destroyAllWindows()
+
+
+def format_text(details):
+    """
+    This function take one argument as
+    input.This function will arrange
+    resulted text into proper format.
+    :param details: dictionary
+    :return: list
+    """
+    parse_text = []
+    word_list = []
+    last_word = ''
+    for word in details['text']:
+        if word != '':
+            word_list.append(word)
+            last_word = word
+        if (last_word != '' and word == '') or (word == details['text'][-1]):
+            parse_text.append(word_list)
+            word_list = []
+
+    return parse_text
+
+
+def write_text(formatted_text):
+    """
+    This function take one argument.
+    it will write arranged text into
+    a file.
+    :param formatted_text: list
+    :return: None
+    """
+    with open('resulted_text.txt', 'w', newline="") as file:
+        csv.writer(file, delimiter=" ").writerows(formatted_text)
+
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+
+        #image = cv2.imread(uploaded_file_url)
+
+        image = cv2.imread(myfile,1)
+
+        thresholds_image = pre_processing(image)
+    
+        parsed_data = parse_text(thresholds_image)
+    
+        accuracy_threshold = 30
+    
+        #draw_boxes(thresholds_image, parsed_data, accuracy_threshold)
+
+        arranged_text = format_text(parsed_data)
+
+        return render(request, 'ocrapp/index.html', {
+            'uploaded_file_url': uploaded_file_url, 'txt': arranged_text
+        })
+
     context = {}
     return render(request, 'ocrapp/index.html', context)
